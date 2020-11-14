4a0d825ecb083665e1d733e78505ce0b0b7ae4ab
gm3k4g <thingstuffet@gmail.com>
2020-11-14 11:44:28 +0200

    Wrote new code, moved old code to different file

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # SDL in a struct
 
-- This is an attempt to put the Rust SDL2 example code within a struct.
+- This is an attempt to break down parts of the Rust SDL2 example code into structs.
+(Check window.rs for the old code)
--- src/main.rs
+++ src/main.rs
@@ -3,50 +3,75 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
  
-pub struct SdlWindow {
+pub struct SdlContext {
 	pub sdl_context: sdl2::Sdl,
-	pub video_subsystem: sdl2::VideoSubsystem,
-	pub canvas: sdl2::render::WindowCanvas,
-	pub event_pump: sdl2::EventPump,
-
 }
-impl SdlWindow {
-	fn new(title: &str, width: u32, height: u32) -> Self {
-
-
-	let sdlc = sdl2::init().unwrap();
-    let videos = sdlc.video().unwrap();
- 
- 	// Consume Window to create WindowCanvas
-    let win = videos.window(title, width, height)
-        .position_centered()
-        .build()
-        .unwrap();
-    let canv = win.into_canvas().build().unwrap();
-
-    let ep = sdlc.event_pump().unwrap();
-
-		SdlWindow {
-			sdl_context: sdlc,
-			video_subsystem: videos,
-       		canvas: canv,
-       		event_pump: ep,
+impl SdlContext {
+	fn new() -> Self {
+		SdlContext {
+			sdl_context: sdl2::init().unwrap(),
 		}
 	}
+}
 
-	fn prepare_canvas(&mut self) {
+pub struct SdlVideoSubsystem {
+	pub video_subsystem: sdl2::VideoSubsystem,
+}
+impl SdlVideoSubsystem {
+	fn new(sdlcontext: sdl2::Sdl) -> Self {
+		SdlVideoSubsystem {
+			video_subsystem: sdlcontext.video().unwrap(),
+		}
+	}
+}
+
+pub struct SdlCanvas {
+	pub canvas: sdl2::render::WindowCanvas,
+}
+impl SdlCanvas {
+	fn new(title: &str, width: u32, height: u32, sdlvideo: sdl2::VideoSubsystem) -> Self {
+	    let win = sdlvideo.window(title, width, height)
+	        .position_centered()
+	        .build()
+	        .unwrap();
+	    let canv = win.into_canvas().build().unwrap();
+	    SdlCanvas {
+	    	canvas: canv,
+	    }
+	}
+
+	fn prepare(&mut self) {
 		self.canvas.set_draw_color(Color::RGB(0, 255, 255));
     	self.canvas.clear();
     	self.canvas.present();
 	}
+}
 
-	fn main_loop(&mut self) {
+pub struct SdlEventPump {
+	pub event_pump: sdl2::EventPump,
+}
+impl SdlEventPump {
+	fn new(sdlcontext: sdl2::Sdl) -> Self {
+		SdlEventPump {
+			event_pump: sdlcontext.event_pump().unwrap(),
+		}
+	}
+}
+
+pub struct SdlLoop {
+	pub sdl_context: sdl2::Sdl,
+	pub video_subsystem: sdl2::VideoSubsystem,
+	pub canvas: sdl2::render::WindowCanvas,
+	pub event_pump: sdl2::EventPump,
+}
+impl SdlLoop {
+	fn new(mut canvas: sdl2::render::WindowCanvas, mut event_pump: sdl2::EventPump) {
 	    let mut i = 0;
 	    'running: loop {
 	        i = (i + 1) % 255;
-	        self.canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-	        self.canvas.clear();
-	        for event in self.event_pump.poll_iter() {
+	        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+	        canvas.clear();
+	        for event in event_pump.poll_iter() {
 	            match event {
 	                Event::Quit {..} |
 	                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
@@ -57,14 +82,18 @@ impl SdlWindow {
 	        }
 	        // The rest of the game loop goes here...
 
-	        self.canvas.present();
+	        canvas.present();
 	        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
 	    }
 	}
 }
 
 fn main() {
-	let mut sdlwindow = SdlWindow::new("Hello", 800, 600);
-	sdlwindow.prepare_canvas();
-	sdlwindow.main_loop();
+	let sdlcontext 					= SdlContext::new();
+	let sdlvideo 					= SdlVideoSubsystem::new(sdlcontext.sdl_context); 
+	let /*mut*/ sdl_event_pump 	    = SdlEventPump::new(sdlvideo.video_subsystem.sdl());
+	let mut sdlcanvas 				= SdlCanvas::new("Hello World", 800, 600, sdlvideo.video_subsystem);
+	sdlcanvas.prepare();
+
+	SdlLoop::new(sdlcanvas.canvas, sdl_event_pump.event_pump);
 }
